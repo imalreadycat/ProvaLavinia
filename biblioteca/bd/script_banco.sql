c0c42ec8a8027006e94782f3621bc6bf36dc06c8
1831321 <1831321@LABCMAQ18.itp.local>
2019-10-17 17:42:58 -0300

    Prova GBD

--- biblioteca/bd/script_banco.sql
+++ biblioteca/bd/script_banco.sql
@@ -16,3 +16,33 @@ DEFAULT CHARACTER SET = utf8
 
 INSERT INTO `mvcd`.`usuario` (`nome`, `senha`, `email`, `papel`) VALUES ('admin', '123', 'admin@admin', 'admin');
 INSERT INTO `mvcd`.`usuario` (`nome`, `senha`, `email`, `papel`) VALUES ('usuario', '123', 'usuario@usuario', 'usuario');
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cadastrarCliente`(IN rg INT(10), nome VARCHAR(60))
+BEGIN
+	IF(rg != 0)AND(nome != "")THEN
+		INSERT INTO cliente (rg, nome) VALUES(rg, nome);
+	ELSE 
+		SELECT "Você deve preencher todos os campos!" AS msg;
+	END IF;
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarClientes`()
+BEGIN
+
+	SELECT * FROM cliente;
+
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deletarClienteId`(IN id INT(10))
+BEGIN
+	    DELETE FROM cliente WHERE idCliente=id;
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `editarCliente`(IN id INT(10), rg INT(10), nome VARCHAR(60))
+BEGIN
+	UPDATE cliente SET rg=rg, nome=nome WHERE idCliente=id;
+END
